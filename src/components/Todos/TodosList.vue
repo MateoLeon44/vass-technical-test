51dbedb8b437bf57661e379bc69dabc461663150
MateoLeon44 <m.leona@uniandes.edu.co>
2022-05-15 00:26:05 -0500

    Added Todos UI without checkboxes

---
+++ src/components/Todos/TodosList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <TodoRender :todos="todos" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TodoRender from "@/components/shared/TodoRender.vue";
+import Todo from "@/types/Todo.class";
+
+export default Vue.extend({
+  components: { TodoRender },
+  computed: {
+    todos(): Todo[] {
+      return this.$store.getters.getTodos;
+    },
+  },
+});
+</script>
+
+<style lang="sass"></style>
