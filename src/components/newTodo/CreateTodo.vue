ec56a8d042e3168537efcc6c4c0d6c788509d656
MateoLeon44 <m.leona@uniandes.edu.co>
2022-05-14 23:12:50 -0500

    Added initial input

---
+++ src/components/newTodo/CreateTodo.vue
@@ -0,0 +1,17 @@
+<template>
+  <input class="new-todo" v-model="todo" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function () {
+    return {
+      todo: "",
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
